55f999846e818389c3d792922d19c3d2ecf58095
Marioo295 <mramosgaliana@gmail.com>
2022-10-03 12:11:55 +0200

    ContextAPI: sesión de usuario

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,8 @@ const Footer = ( {theme, texts} ) => {
     return (
         <footer className={theme}>
             <h4>{texts.footerTitle}</h4>
+            <p>{texts.footerContent1}</p>
+            <p>{texts.footerContent2}</p>
         </footer>
     );
 };
--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -5,6 +5,7 @@ import Main from './Main';
 
 const initialTheme = "light";
 const initailLanguage = "es";
+const initialAuth=null;
 const translations = {
     es:{
         headerTitle: "Mi aplicación sin ContextAPI",
@@ -13,10 +14,12 @@ const translations = {
         headerDark: "Oscuro",
         buttonLogin: "Iniciar sesión",
         buttonLogout: "Cerrar sesión",
-        mainWelcome: "Bienvenid@ invitad@",
+        mainWelcome: "Bienvenid@ invitad@ a la aplicación",
         mainHello: "Hola usuari@",
-        mainContent: "Mi contenido principal",
+        mainContent: "Este es el contenido de la aplicación sin ContextAPI de Mario",
         footerTitle: "Mi pie de página",
+        footerContent1: "Contacto: Marioo295",
+        footerContent2: "Número: 677385218",
     },
     en:{
         headerTitle: "My application without ContextAPI",
@@ -25,10 +28,12 @@ const translations = {
         headerDark: "Dark",
         buttonLogin: "Login",
         buttonLogout: "Logout",
-        mainWelcome: "Welcome",
+        mainWelcome: "Welcome to the application",
         mainHello: "Hello user",
-        mainContent: "My main content",
+        mainContent: "This is the context of the application without ContextAPI of Mario",
         footerTitle: "My footer",
+        footerContent1: "Contact: Marioo295",
+        footerContent2: "Number: 677385218",
     },
 };
 
@@ -37,6 +42,7 @@ const MyPage = () => {
     const [theme, setTheme] = useState(initialTheme);
     const [language, setLanguage] = useState(initailLanguage);
     const [texts, setTexts] = useState(translations[language]);
+    const [auth, setAuth] = useState(initialAuth);
 
     const handleTheme = (e) => {
         //console.log(e.target.value);
@@ -58,10 +64,18 @@ const MyPage = () => {
         }
     };
 
+    const handleAuth = (e) => {
+        if (auth) {
+            setAuth(null);
+        } else {
+            setAuth(true);
+        }
+    };
+
     return (
         <div className="my-page">
-            <Header theme={theme} handleTheme={handleTheme} texts={texts} handleLanguage={handleLanguage}></Header>
-            <Main theme={theme} texts={texts}></Main>
+            <Header theme={theme} handleTheme={handleTheme} texts={texts} handleLanguage={handleLanguage} auth={auth} handleAuth={handleAuth}></Header>
+            <Main theme={theme} texts={texts} auth={auth}></Main>
             <Footer theme={theme} texts={texts}></Footer>
         </div>
     );
