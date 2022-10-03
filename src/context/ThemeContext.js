520460f9a4e7a29fe94c07eb73d1ffa0ce7a3ab1
Marioo295 <mramosgaliana@gmail.com>
2022-10-03 13:32:31 +0200

    ContextAPI: con context terminado

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -8,7 +8,7 @@ const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(initialTheme);
 
   const handleTheme = (e) => {
-    console.log(e.target.value);
+    //console.log(e.target.value);
     if (e.target.value === "light") {
       setTheme("light");
     } else {
