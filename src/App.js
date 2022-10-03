9084b434caead0819e2d1627241b94ce6a1e13a9
Marioo295 <mramosgaliana@gmail.com>
2022-10-03 10:56:03 +0200

    ContextAPI

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import MyPage from './components/MyPage';
+
+function App() {
+    return (
+        <div>
+            <h1>REACT CONTEXT API</h1>
+            <a
+                href="https://es.reactjs.org/docs/context.html"
+                target="_blank"
+                rel="noreferrer"
+            >Documentación ContextAPI</a>
+            <hr></hr>
+            <MyPage></MyPage>
+            <hr></hr>
+        </div>
+    );
+};
+
+export default App;
